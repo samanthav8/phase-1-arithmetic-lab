9c87a310c331efee9149cf23215665d417633fdb
Samantha Villafana <samanthavillafana8@gmail.com>
2023-11-05 12:39:23 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100);
+
+let num3 = 20;
+let num4 = 8;
+const mod = num3 % num4;
+
+const numberSet = [0,5,10,15,20];
+const max = Math.max(...numberSet)
+
